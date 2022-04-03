b5b356c0df87d75c132a8aef5b21a1aa4a407dd4
adamhatton <adamdhatton@gmail.com>
2022-04-03 10:16:37 +0000

    Add redesigned function to validate user input

--- run.py
+++ run.py
@@ -1,15 +1,16 @@
 import sys
 
-print(
-    '''
-  ____       _______ _______ _      ______    _____ _    _ _____ _____   _____ 
- |  _ \   /\|__   __|__   __| |    |  ____|  / ____| |  | |_   _|  __ \ / ____|
- | |_) | /  \  | |     | |  | |    | |__    | (___ | |__| | | | | |__) | (___  
- |  _ < / /\ \ | |     | |  | |    |  __|    \___ \|  __  | | | |  ___/ \___ \ 
- | |_) / ____ \| |     | |  | |____| |____   ____) | |  | |_| |_| |     ____) |
- |____/_/    \_\_|     |_|  |______|______| |_____/|_|  |_|_____|_|    |_____/ 
-    '''
-)
+# print(
+#     '''
+#   ____       _______ _______ _      ______    _____ _    _ _____ _____   _____ 
+#  |  _ \   /\|__   __|__   __| |    |  ____|  / ____| |  | |_   _|  __ \ / ____|
+#  | |_) | /  \  | |     | |  | |    | |__    | (___ | |__| | | | | |__) | (___  
+#  |  _ < / /\ \ | |     | |  | |    |  __|    \___ \|  __  | | | |  ___/ \___ \ 
+#  | |_) / ____ \| |     | |  | |____| |____   ____) | |  | |_| |_| |     ____) |
+#  |____/_/    \_\_|     |_|  |______|______| |_____/|_|  |_|_____|_|    |_____/ 
+#     '''
+# )
+
 
 def welcome_message():
     '''
@@ -17,31 +18,34 @@ def welcome_message():
     collects user input
     '''  
     while True:
-        print('Would you like to play a game?\n')
-        print('[y] = yes, [n] = no\n')
+        print('Would you like to play a game?')
+        print('[y] = yes, [n] = no')
 
-        answer = input('\n')
+        player_answer = input('\n').lower()
 
-        if validate_input(answer):
-            print('yay')
+        if validate_input(player_answer):
             break
 
-    return answer
+    return player_answer
 
 
 def validate_input(input):
+    '''
+    Validates user input and returns True if it is valid or False if it is not
+    '''
     try:
-        input.lower()
-    
-    except AttributeError:
-        print('Invalid input, please try again')
+        if input == 'y' or input == 'n':
+            return True
+        else:
+            raise Exception
+    except Exception:
+        print("Input must be 'y' or 'n', please try again\n")
         return False
-    
     return True
-
 
 
 def main():
     welcome_message()
+    print('code got to main()')
 
 main()
